3e8c3d8e024f3e6af011ae4c7a086d5da6e62ad1
Ashwin <ashwin@askerbot.com>
2023-02-23 21:36:16 +0530

    updated app.js

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/krishna', function(req, res, next) {
+  res.send('respond with a krishna');
+});
+router.get('/fidha', function(req, res, next) {
+  res.send('respond with a fidha');
+});
+router.get('/karthika', function(req, res, next) {
+  res.send('respond with a karthika');
+});
+router.get('/karthika/devika', function(req, res, next) {
+  res.send('respond with a devika');
 });
 
 module.exports = router;
