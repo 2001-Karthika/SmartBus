8ad397d0b8755e882b827f30f77dc0ace695295d
Ashwin <ashwin@askerbot.com>
2023-02-23 22:48:57 +0530

    Add login page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/karthika', function(req, res) {
-  res.render('login.html', { titles: 'Fidha' });
+router.get('/login', function(req, res) {
+  res.render('login/login.ejs', { titles: 'Fidha' });
 });
 
 module.exports = router;
