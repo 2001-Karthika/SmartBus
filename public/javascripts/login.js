149c56f41186cf9a9899f8d62009ecacafe41ce7
Krishna <krishnagaadhamh@gmail.com>
2023-02-24 15:42:54 +0530

    MAde changes

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,11 +1,19 @@
-//const username= document.getElementById();
-/*document.querySelector('form.pure-form').addEventListener("submit", get-value(e){
-  e.preventDefault();
-  console.log(username.value);});*/
-console.log("hello")
 function get_value(){
   const userType= document.getElementById("user-type").value
   const username= document.getElementById("username").value
   const password= document.getElementById("password").value
   console.log(userType, username, password)
+  alert("hello")
+  fetch('/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+})
+.then((response) => response.json())
+.then((response)=>{
+  console.log(response)
+  alert("hello world")
+})
+
 }
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/login', function(req, res) {
-  res.render('login/login.ejs', { titles: 'Fidha' });
+  res.render('login/login.ejs');
+});
+router.post('/login', function(req, res) {
+  res.status(200).send({success: false, error: {message: 'No blah Found'}});
+});
+router.get('/dashboard', function(req, res) {
+  res.status(200).send({success: false, error: {message: 'No blah Found'}});
 });
 
 module.exports = router;
