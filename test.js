c6f7de22d1ed3b7e04f4c7b2c3c8dcea0ffd03af
Karthika <karthikastech@gmail.com>
2023-03-05 23:33:14 +0530

    new branch for dashboard

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const runQuery  = require('./db/runQuery');
 const a = 'INSERT INTO `smartbus`.`admin` (`name`, `username`, `password`, user_type, `email`, `ph_num`) VALUES (?,?,?,?,?,?);'
-const b = 'ashwin'
-const c = 'ashwin'
-const d = '123456'
-const e = 'test@gmail.com'
-const f = '7994771185'
-const g = 1
-async function main(a,b,c,d,e,f){
+const b = 'admin D'
+const c = 'admind'
+const d = 'sffnk'
+const e = 'admind@gmail.com'
+const f = '987532948'
+const g = 0
+async function main(a,b,c,d,g,e,f){
     const res = await runQuery(a,[b,c,d,g,e,f])
     console.log(res)
 }
