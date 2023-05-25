641d1c5f1985a2d2cccdd4377c4ded23e0bdc6f4
Ashwin <ashwin@askerbot.com>
2023-05-25 21:12:37 +0530

    Modified gps data

--- gpsgeocoder.js
+++ gpsgeocoder.js
@@ -2,6 +2,7 @@ const NodeGeocoder = require('node-geocoder');
 const firebaseLib = require("firebase-admin");
 const accountDetails = require('./keytofirebase.json')
 const IORedis = require('ioredis')
+const axios = require('axios');
 const redisIOClient = new IORedis()
 const runQuery = require('./db/runQuery')
 
@@ -12,57 +13,69 @@ const getFirebaseConnection = () => {
   })
 }
 
-const callbackService = () => {
-  const dbPath = 'd74acef3-7630-484c-b82f-8908bef34c50'
-
-  const options = {
-    provider: 'openstreetmap'
-  };
-  const geocoder = NodeGeocoder(options);
-
-  // Fetch the latitude and longitude from Firebase
-  firebaseLib.database().ref(dbPath).once('value')
-    .then(snapshot => {
-      const data = snapshot.val()
-      let latitude = data?.latitude
-      let longitude = data?.longitude
-      let noOfPerson = data?.noOfPerson
-      // Call the geocoder.reverse method with the retrieved latitude and longitude
-      async function getLocFromGeocoder(latitude, longitude) {
-        try {
-          const result = await geocoder.reverse({ lat: latitude, lon: longitude })
-          const loc = result[0].formattedAddress.split(',')[0]
-          return loc;
-        } catch (error) {
-          console.error(error);
-        }
+async function getLocFromGeocoder(latitude, longitude) {
+  try {
+    const options = {
+      method: 'GET',
+      url: 'https://feroeg-reverse-geocoding.p.rapidapi.com/address',
+      params: {
+        lat: latitude,
+        lon: longitude,
+        lang: 'en',
+        mode: 'text',
+        format: '\'[SN[, ] - [23456789ab[, ]\''
+      },
+      headers: {
+        'X-RapidAPI-Key': '567d017794msh0d3e882efdce044p112a1fjsnca9708f983bf',
+        'X-RapidAPI-Host': 'feroeg-reverse-geocoding.p.rapidapi.com'
       }
-      const update = async () => {
-        try {
-          const currentLoc = await getLocFromGeocoder(latitude, longitude)
-          const values = [noOfPerson, currentLoc, dbPath]
-          redisIOClient.hmset('smartbus_settings', 'no_of_persons', noOfPerson??0,
-                                                    'current_location', currentLoc??'',
-                                                    )
-          console.log(values);
-          const query = `Update bus set crowd = ? , currentLoc = ? where uuid = ?`;
-          //add redis update
-          await runQuery(query, values)
-        } catch (err) {
-          console.log(err.message)
-        }
+    }
+    const response = await axios.request(options)
+    return response?.data
+  } catch (error) {
+    console.error(error.message)
+  }
+}
 
-      };
-      update();
+const callbackService = async () => {
+  try {
+    const dbPath = 'd74acef3-7630-484c-b82f-8908bef34c50';
+    // Fetch the latitude and longitude from Firebase
+    const snapshot = await firebaseLib.database().ref(dbPath).once('value');
+    const data = snapshot.val()
+    let latitude = data?.latitude
+    let longitude = data?.longitude
+    let noOfPerson = data?.noOfPerson
+    let currentLoc = '';
+    try {
+      currentLoc = await getLocFromGeocoder(latitude, longitude)
+    } catch (err) {
+      console.log('Error fetching location from geocoder:', err);
+    }
+    const dataPoints = [noOfPerson, currentLoc, dbPath];
+    console.log({values: dataPoints})
+    if (currentLoc)
+      redisIOClient.hmset(
+        'smartbus_settings',
+        'no_of_persons',
+        noOfPerson ?? 0,
+        'current_location',
+        currentLoc ?? ''
+      )
+    const query = 'UPDATE bus SET crowd = ?, currentLoc = ? WHERE uuid = ?';
 
-    })
-
+    await runQuery(query, dataPoints);
+  } catch (err) {
+    console.log(err);
+  }
 };
+
+
 const main = () => {
   getFirebaseConnection()
   setInterval(() => {
     callbackService()
-  }, 3000)
+  }, 5000)
 }
 main()
 
